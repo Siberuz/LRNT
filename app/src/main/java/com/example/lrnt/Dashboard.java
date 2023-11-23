3f920f69bdb174248a26f009c69f730340fd490f
Siberuz <ruzz.arianto@gmail.com>
2023-11-23 23:59:40 +0700

    commit3

---
+++ app/src/main/java/com/example/lrnt/Dashboard.java
@@ -0,0 +1,18 @@
+package com.example.lrnt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Dashboard extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.dashboard_layout, container, false);
+    }
+}
--- app/src/main/java/com/example/lrnt/MainActivity.java
+++ app/src/main/java/com/example/lrnt/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.lrnt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    Dashboard dashboardFragment =  new Dashboard();
+    Course courseFragment = new Course();
+    Profile profileFragment = new Profile();
+    Ranks ranksFragment = new Ranks();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.nav_bottom);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, dashboardFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.dashboard_menu){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, dashboardFragment).commit();
+                    return true;
+                }
+                if(id == R.id.course_menu){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, courseFragment).commit();
+                    return true;
+                }
+                if(id == R.id.profile_menu){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, profileFragment).commit();
+                    return true;
+                }
+                if(id == R.id.ranks_menu){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, ranksFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
+
 }
