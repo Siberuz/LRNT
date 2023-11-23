b69c5e3e2162450b9df1faa05ed357ae9e109bf1
Priyotama <chantyo.atmaja@gmail.com>
2023-11-23 23:11:54 +0700

    sambungin login backend ke view

--- app/src/main/java/com/example/lrnt/Login.java
+++ app/src/main/java/com/example/lrnt/Login.java
@@ -3,9 +3,10 @@ package com.example.lrnt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    EditText 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
