ef309ee8cbcf76443ff871688126c4a93037c456
Priyotama <chantyo.atmaja@gmail.com>
2023-11-23 23:18:29 +0700

    Update Login.java

--- app/src/main/java/com/example/lrnt/Login.java
+++ app/src/main/java/com/example/lrnt/Login.java
@@ -3,13 +3,35 @@ package com.example.lrnt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
-    EditText 
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    EditText emailET;
+    EditText passwordET;
+    Button loginButton;
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        emailET = findViewById(R.id.login_email);
+        passwordET = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        registerButton = findViewById(R.id.loginPage_registerButton);
+
+        loginButton.setOnClickListener(this);
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == loginButton){
+            //logic login
+        }
+        if(v == registerButton){
+            //pindah ke register page
+        }
     }
 }
