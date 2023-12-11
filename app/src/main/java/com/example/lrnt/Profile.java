7fdaae71ae434e75f829c23bda2692322f57f314
Priyotama <chantyo.atmaja@gmail.com>
2023-12-11 23:39:07 +0700

    edit Profile and add Logout Button

--- app/src/main/java/com/example/lrnt/Profile.java
+++ app/src/main/java/com/example/lrnt/Profile.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,12 +23,19 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
-public class Profile extends Fragment {
+public class Profile extends Fragment implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private TextView tv_name;
     private TextView tv_email;
+    private EditText email_et;
+    private ImageButton edit_email;
+    private ImageButton edit_email_done;
+    private TextView username_tv;
+    private EditText username_et;
+    private ImageButton edit_username;
+    private ImageButton edit_username_done;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,6 +47,18 @@ public class Profile extends Fragment {
 
         tv_name = v.findViewById(R.id.profileName);
         tv_email = v.findViewById(R.id.Email_profile_id);
+        edit_email = v.findViewById(R.id.edit_email_button);
+        email_et = v.findViewById(R.id.profile_email_et);
+        edit_email.setOnClickListener(this);
+        edit_email_done = v.findViewById(R.id.edit_email_button_done);
+        edit_email_done.setOnClickListener(this);
+
+        username_tv = v.findViewById(R.id.username_profile_id);
+        username_et = v.findViewById(R.id.profile_username_et);
+        edit_username = v.findViewById(R.id.edit_username_button);
+        edit_username.setOnClickListener(this);
+        edit_username_done = v.findViewById(R.id.edit_username_button_done);
+        edit_username_done.setOnClickListener(this);
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
@@ -69,5 +90,39 @@ public class Profile extends Fragment {
 
         }
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == edit_email){
+            tv_email.setVisibility(View.GONE);
+            email_et.setVisibility(View.VISIBLE);
+            edit_email.setVisibility(View.GONE);
+            edit_email_done.setVisibility(View.VISIBLE);
+            email_et.setText(tv_email.getText().toString());
+        }
+        if(v == edit_email_done){
+            String newEmail = email_et.getText().toString();
+            tv_email.setText(newEmail);
+            tv_email.setVisibility(View.VISIBLE);
+            email_et.setVisibility(View.GONE);
+            edit_email.setVisibility(View.VISIBLE);
+            edit_email_done.setVisibility(View.GONE);
+        }
+        if(v == edit_username){
+            username_tv.setVisibility(View.GONE);
+            username_et.setVisibility(View.VISIBLE);
+            edit_username.setVisibility(View.GONE);
+            edit_username_done.setVisibility(View.VISIBLE);
+            username_et.setText(username_tv.getText().toString());
+        }
+        if(v == edit_username_done){
+            String newUsername = username_et.getText().toString();
+            username_tv.setText(newUsername);
+            username_tv.setVisibility(View.VISIBLE);
+            username_et.setVisibility(View.GONE);
+            edit_username.setVisibility(View.VISIBLE);
+            edit_username_done.setVisibility(View.GONE);
+        }
     }
 }
